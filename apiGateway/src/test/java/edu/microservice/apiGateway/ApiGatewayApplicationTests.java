6e0c7446ce3a90287584eb49097216a6742e5af1
ZelihaArslan <arslanzeliha00@gmail.com>
2025-12-06 12:17:39 +0300

    add apiGateway

---
+++ apiGateway/src/test/java/edu/microservice/apiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.microservice.apiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
